aa0b22b2e75694259a6263cd06ff8d8002e00eda
Vu Hoang <hoangvubrvt@gmail.com>
2018-01-06 14:40:22 +0700

    Changed version of app on docker

--- build-dev.sh
+++ build-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export DOCKER_TAG=dev
-docker build -t up2university/learninglocker2-app:$DOCKER_TAG app
-docker build -t up2university/learninglocker2-nginx:$DOCKER_TAG nginx
+export DOCKER_TAG=master
+docker build -t hoangvubrvt/learninglocker:$DOCKER_TAG app
+docker build -t hoangvubrvt/learninglocker:$DOCKER_TAG nginx
 
