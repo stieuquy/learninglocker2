4ca0064ace32f628854157609f1b803071803621
Vu Hoang <hoangvubrvt@gmail.com>
2018-02-27 06:14:01 +0700

    Used the latest source code of LearningLocker and XAPI

--- build-dev.sh
+++ build-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export DOCKER_TAG=master
+export DOCKER_TAG=latest
 docker build -t hoangvubrvt/learninglocker:$DOCKER_TAG app
 docker build -t hoangvubrvt/learninglocker:$DOCKER_TAG nginx
 
